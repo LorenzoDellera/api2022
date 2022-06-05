d1f5057675b094adaa912d32e3af4b679b05dae6
LorenzoDellera <100216966+LorenzoDellera@users.noreply.github.com>
2022-06-05 19:48:10 +0200

    first list implementation

--- main.c
+++ main.c
@@ -1,10 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #define leng 100
 
+//STRUTTURA
+typedef struct Lista *lista_punt;
+struct Lista {
+    char word[leng];
+    int t0f1;
+    lista_punt next;
+};
+
+//INSERIMENTO IN LISTA
+lista_punt inserisci(lista_punt lista, char word[leng]) {
+    lista_punt  new = NULL;
+    new = (lista_punt) malloc(sizeof (struct Lista));
+    strcpy(new->word,word);
+    new->next = lista;
+    return new;
+}
+
+//STAMPA LISTA
+void stampa(lista_punt lista) {
+    if (lista->next != NULL) {
+        stampa(lista->next);
+    }
+    printf("%s",lista->word);
+}
+
+//PROGRAMMA
 int main() {
     char string_prova[leng];
     int string_lenght, count=0;
     int disable = 0;
+    lista_punt lista = NULL;
 
     scanf("%d",&string_lenght);
     fgets(string_prova,leng,stdin);
@@ -13,7 +42,7 @@ int main() {
             disable = 1;
         }
         if(disable==0) {
-            puts(string_prova);
+            lista = inserisci(lista,string_prova);
         }
         if(disable==0) {
             count++;
@@ -26,6 +55,7 @@ int main() {
             }
         }*/
     }
+    stampa(lista);
     printf("parole iniziali = %d",count);
     printf("\ndi lunghezza = %d",string_lenght);
 }
